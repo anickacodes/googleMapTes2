e10b3ff39d85ff0c7b19944d030bed2164b2709c
anickacodes <anickalewis@pursuit.org>
2024-01-27 21:46:33 -0500

    enhance UX for route updates(state)

--- src/components/MapVgi.jsx
+++ src/components/MapVgi.jsx
@@ -5,14 +5,15 @@ import {
   useMap,
 } from "@vis.gl/react-google-maps";
 import { useEffect, useState } from "react";
-import '../App.css'
-
-
+import "../App.css";
 
 const MapVgi = () => {
   const position = { lat: 40.6868, lng: -73.9557 };
   return (
-    <div className="map2-container" style={{ height: "100vh", width: "100%" }}>
+    <div
+      className="map2-container"
+      style={{ height: "100vh", width: "100%", padding: "10px" }}
+    >
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
         <Map
           center={position}
@@ -62,15 +63,30 @@ function Directions() {
   }, [directionsService, directionsRenderer]);
   console.log(routes);
 
+  useEffect(() => {
+    if (!directionsRenderer) return;
+    directionsRenderer.setRouteIndex(routeIndex);
+  }, [routeIndex, directionsRenderer]);
+
   if (!leg) return null;
   return (
     <div className="directions">
       <h2>{selected.summary}</h2>
       <p>
-        {leg.start_address.split(',')[0]} to {leg.end_address.split(',')[0]}
+        {leg.start_address.split(",")[0]} to {leg.end_address.split(",")[0]}
       </p>
       <p>Distance: {leg.distance?.text}</p>
       <p>Duration: {leg.duration?.text}</p>
+      <h2>Other Routes Available</h2>
+      <ul>
+        {routes.map((route, index) => (
+          <li key={route.summary}>
+            <button onClick={() => setRouteIndex(index)}>
+              {route.summary}
+            </button>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
